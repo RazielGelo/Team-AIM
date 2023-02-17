3d6b5288e386701730035c3db2a8398ad89fd8e3
MiykelC <105033752+MiykelC@users.noreply.github.com>
2023-02-17 01:13:38 -0800

    Pushing my part of the code and aditional css

---
+++ pre/src/Applications.js
@@ -0,0 +1,128 @@
+
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Nav from "./components/Nav";
+import Carousel from "./components/Carousel";
+import CompanionCard from "./components/CompanionCard";
+import "./styles/Applications.css";
+
+function Applications() {
+  const applications = JSON.parse(localStorage.getItem("applications")) || [];
+  const selectedRequest = JSON.parse(localStorage.getItem("selectedRequest"));
+  const [location, setLocation] = useState();
+  const [errorMessage, setErrorMessage] = useState("");
+
+
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    const session = JSON.parse(localStorage.getItem("session"));
+    if (!session) {
+      return navigate("/");
+    }
+
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    const success = (pos) => {
+      const crd = pos.coords;
+
+      setLocation({
+        lat: crd.latitude,
+        lng: crd.longitude,
+      });
+    };
+
+    function error(err) {
+      setErrorMessage(
+        `ERROR(${err.code}): ${err.message}. Error getting your location`
+      );
+    }
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }, []);
+
+  const handleAcceptClick = (application) => {
+    const session = JSON.parse(localStorage.getItem("session")) || {};
+    const accounts = JSON.parse(localStorage.getItem("accounts")) || [];
+
+    const updatedApplication = { ...application, status: "accepted" };
+    const filteredApplications = applications.filter(
+      (a) => a.companionEmail !== updatedApplication.companionEmail
+    );
+    filteredApplications.push(updatedApplication);
+    localStorage.setItem("applications", JSON.stringify(filteredApplications));
+
+    const user = accounts.find((account) => account.email === session.email);
+    const userRequest = user.requests.find(
+      (r) => r.taskName === selectedRequest.taskName
+    );
+    userRequest.status = "in-progress";
+
+    const filteredAccounts = accounts.filter(
+      (account) => account.email !== session.email
+    );
+    const updatedUser = {
+      ...user,
+      requests: user.requests.map((request) =>
+        request.taskName === selectedRequest.taskName
+          ? {
+            ...request,
+            status: "in-progress",
+            applications: [
+              ...(request.applications || []).filter(
+                (a) => a.companionEmail !== updatedApplication.companionEmail
+              ),
+              updatedApplication,
+            ],
+            location: {
+              lng: -122.950891, // location.lng
+              lat: 49.215401, // location.lat
+            }
+          }
+          : request
+      ),
+    };
+    filteredAccounts.push(updatedUser);
+    localStorage.setItem("accounts", JSON.stringify(filteredAccounts));
+    localStorage.setItem("session", JSON.stringify(updatedUser));
+    return navigate("/yourrequests");
+  };
+
+  return (
+    <div className="applications-container">
+      <Nav />
+
+      <div className="applications-sub1">
+        <h1>Applications - {selectedRequest.taskName}</h1>
+      </div>
+
+      <div className="applications-sub2">
+        <div className="applications-carousel">
+          <Carousel>
+            {applications
+              .filter((application) => application.status === "waiting")
+              .map((application, index) => (
+                <CompanionCard
+                  key={application.companionEmail + index}
+                  email={application.companionEmail}
+                  name={application.companionName}
+                  age={application.companionAge}
+                  ethnicity={application.companionEthnicity}
+                  gender={application.companionGender}
+                  phonenumber={application.companionPhone}
+                  fee={application.fee}
+                  description={application.description}
+                  onButtonClick={() => handleAcceptClick(application)}
+                />
+              ))}
+          </Carousel>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Applications;
---
+++ pre/src/Profile.js
@@ -0,0 +1,211 @@
+
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import WhiteInput from "./components/WhiteInput";
+import YellowButton from "./components/YellowButton";
+import RadioButton from "./components/RadioButton";
+import Nav from "./components/Nav";
+import "./styles/Profile.css";
+import nameLogo from "./images/name.png";
+import ethnicityLogo from "./images/ethnicity.png";
+import ageLogo from "./images/age.png";
+import phoneLogo from "./images/phonenumber.png";
+import femaleLogo from "./images/girl.png";
+import maleLogo from "./images/boy.png";
+import otherLogo from "./images/other.png";
+function Profile() {
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
+
+  let navigate = useNavigate();
+
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    name: "",
+    ethnicity: "",
+    age: "",
+    gender: "",
+    phoneNumber: "",
+  });
+
+  useEffect(() => {
+    if (successMessage) {
+      setShowSuccessMessage(true);
+      setTimeout(() => {
+        setShowSuccessMessage(false);
+        return navigate("/");
+      }, 3000);
+    }
+  }, [successMessage, navigate]);
+
+  useEffect(() => {
+    if (errorMessage) {
+      setShowErrorMessage(true);
+      setTimeout(() => {
+        setShowErrorMessage(false);
+      }, 3000);
+    }
+  }, [errorMessage]);
+
+  const handleLogout = () => {
+    localStorage.removeItem("session");
+    return navigate("/");
+  };
+
+  const handleSubmit = () => {
+    const element = document.getElementById("ref");
+    if (element) {
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+    if (!user.name) {
+      setShowErrorMessage(true);
+      setErrorMessage("Please enter your name.");
+      return;
+    }
+
+    if (!user.ethnicity) {
+      setShowErrorMessage(true);
+      setErrorMessage("Please enter your ethnicity.");
+      return;
+    }
+    if (!user.age) {
+      setShowErrorMessage(true);
+      setErrorMessage(`Please your age.`);
+      return;
+    }
+    if (!user.gender) {
+      setShowErrorMessage(true);
+      setErrorMessage(`Please select your gender".`);
+      return;
+    }
+    if (!user.phoneNumber) {
+      setShowErrorMessage(true);
+      setErrorMessage(`Please enter a contact number.`);
+      return;
+    }
+
+    if (!validatePhoneNumber(user.phoneNumber)) {
+      setShowErrorMessage(true);
+      setErrorMessage(
+        "Invalid phone number format. Please enter a valid phone number."
+      );
+      return;
+    }
+
+    const accounts = JSON.parse(localStorage.getItem("accounts")) || [];
+    const session = JSON.parse(localStorage.getItem("session")) || {};
+    const filteredAccounts = accounts.filter(
+      (account) => account.email !== session.email
+    );
+    const updatedUser = {
+      email: user.email,
+      password: user.password,
+      name: user.name,
+      ethnicity: user.ethnicity,
+      gender: user.gender,
+      age: user.age,
+      phoneNumber: user.phoneNumber,
+      requests: [],
+      jobs: [],
+    };
+    filteredAccounts.push(updatedUser);
+    localStorage.setItem("accounts", JSON.stringify(filteredAccounts));
+    localStorage.setItem(
+      "session",
+      JSON.stringify({
+        email: updatedUser.email,
+        password: updatedUser.password,
+      })
+    );
+    setShowSuccessMessage(true);
+    setErrorMessage("");
+    setSuccessMessage("Edit profile successful!");
+  };
+
+  const validatePhoneNumber = (phoneNumber) => {
+    const phoneNumberRegEx = /^\d{3}-\d{3}-\d{4}$/;
+    return phoneNumberRegEx.test(phoneNumber);
+  };
+
+  return (
+    <div className="profile-container">
+      <Nav />
+      <div className="profile-sub1">
+        <h3 id="ref">YOUR PROFILE</h3>
+        <p>
+          You can view or edit your profile here and also log out.
+        </p>
+        {showErrorMessage && <p id="create-error-message">{errorMessage}</p>}
+        {showSuccessMessage && (
+          <p id="create-success-message">{successMessage}</p>
+        )}
+        <WhiteInput
+          type="text"
+          value={user.name}
+          onChange={(e) => setUser({ ...user, familyNickname: e.target.value })}
+          placeholder="Name"
+          logo={nameLogo}
+        />
+        <WhiteInput
+          type="text"
+          value={user.ethnicity}
+          onChange={(e) => setUser({ ...user, ethnicity: e.target.value })}
+          placeholder="Ethnicity"
+          logo={ethnicityLogo}
+        />
+        <WhiteInput
+          type="text"
+          value={user.age}
+          onChange={(e) => setUser({ ...user, ethnicity: e.target.value })}
+          placeholder="Age"
+          logo={ageLogo}
+        />
+        <WhiteInput
+          type="text"
+          value={user.phoneNumber}
+          onChange={(e) => setUser({ ...user, phoneNumber: e.target.value })}
+          placeholder="Enter your phonenumber"
+          logo={phoneLogo}
+        />
+
+        <div className="profile-gender">
+          <p>Gender</p>
+          <RadioButton
+            logo={femaleLogo}
+            onChange={(e) => setUser({ ...user, gender: e.target.value })}
+            value="Girl"
+            checked={user.gender === "Girl"}
+          />
+          <RadioButton
+            logo={maleLogo}
+            onChange={(e) => setUser({ ...user, gender: e.target.value })}
+            value="Boy"
+            checked={user.gender === "Boy"}
+          />
+          <RadioButton
+            logo={otherLogo}
+            onChange={(e) => setUser({ ...user, gender: e.target.value })}
+            value="Other"
+            checked={user.gender === "Other"}
+          />
+        </div>
+        <div className="profile-sub2">
+          <YellowButton id="save" onClick={handleSubmit}>
+            Save
+          </YellowButton>
+          <YellowButton id="logout" onClick={handleLogout}>
+            Logout
+          </YellowButton>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
